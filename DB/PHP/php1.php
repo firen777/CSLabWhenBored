307918aee0031fedaadf585fa4760a984ed0dfa6
firen777 <hivemindac1995@gmail.com>
2017-07-04 21:19:24 +0800

    PHP Derek's

--- DB/PHP/php1.php
+++ DB/PHP/php1.php
@@ -14,6 +14,55 @@
     echo "<ul>
       <li><em><strong>Hello World</strong></em></li>
     </ul>";
+
+    date_default_timezone_set("UTC");
+
+    echo date('h:i:s:u a, l F jS Y e') . "<br/>";
+
+    #Var declaration: $...
+    $name = $_POST['name'];
+    $pw = $_POST['pw'];
+
+    #String concatanation
+    echo $name . " " . $pw . "<br/>";
+
+    #heredoc String
+    $str = <<<EOD
+    <h2><a href="http://php.net/manual/en/language.types.string.php#language.types.string.syntax.heredoc">Heredoc for php string</a></h2>
+    and for var. <br/>
+    Name: $name <br/>
+    PW: $pw <br/>
+EOD;
+
+    echo $str;
+
+    #Constant definition
+    define('PI', 3.2);
+
+    echo "PI: " . PI . "<br/>";
+
+    #Reference
+    $num = 5;
+    $nRef= &$num;
+
+    echo 'Reference of Num: $nRef == ' . $nRef++ . "<br/>"; //escaping $var with SINGLE QUOTE
+    echo 'After $nRef++, $num == ' . $num . "<br/>";
+
+    #Logical operation
+    if ($num==5) {
+      echo 'wut? <br/>'; //false
+    } elseif ($num==6){
+      echo "kek <br/>";
+      echo '$num == 6:' . ($num==6) . "<br/>";
+      echo '$num >= 5:' . ($num>=5) . "<br/>";
+      echo '$num>6 || $num<6:' . ($num>6||$num<6) . "<br/>";
+      echo '$num>=5 && $num<=1000:' . ($num>=5 && $num<=1000) . "<br/>";
+    } elseif ($num!=5) {
+      echo "last elseif <br/>"; //unreached
+    }
+
      ?>
+
+
   </body>
 </html>
